7ca6276e2ddc91c617950ef6d00ebc3fde71531d
Nurzhigit Zhakypbaev <zhakypbaev.n4489@gmail.com>
2023-06-22 11:34:57 +0600

    partners adaptive

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -14,7 +14,52 @@ $('#partners').slick({
   slidesToScroll: 1,
   autoplay: true,
   speed: 500,
+  dots: false,
+  arrows: false,
+  centerPadding: '70px',
   autoplaySpeed: 10000,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      dots: true,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 380,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 $('#categories-list').slick({
